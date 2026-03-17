509d70adb7103c64a1f89b5acac25dfbf6dd9026
Marius Gassen <mariusgassen@users.noreply.github.com>
2026-03-17 22:12:32 +0100

    Add scheduled_at column to scheduled_evening table
    Fix escaping of colon

--- backend/alembic/versions/026_scheduled_evening_scheduled_at.py
+++ backend/alembic/versions/026_scheduled_evening_scheduled_at.py
@@ -19,7 +19,7 @@ def upgrade():
     op.add_column("scheduled_evening", sa.Column("scheduled_at", sa.DateTime(timezone=True), nullable=True))
     op.execute(
         sa.text(
-            r"""
+            """
             UPDATE scheduled_evening
             SET scheduled_at = (
                 date || ' ' || COALESCE(time, '20:00') || '\\:00'
